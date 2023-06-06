de818483a4269eedb9fcdaa4a2d0aa4d839b40d4
sayeed083 <sayeedislam407@gmailcom>
2023-06-06 22:26:57 +0600

    Added Footer and React Icons

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,26 @@
-
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin  } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is FOoter</h2>
+            <footer className="footer footer-center p-10 bg-teal-500 text-primary-content">
+                <div>
+                    
+                    <p className="font-bold">
+                        Summer Camp <br />Providing Education For Your Children Since 2002
+                    </p>
+                    <p>Copyright ©SummerCamp  2023 - All right reserved</p>
+                </div>
+                <div>
+                    <div className="grid grid-flow-col gap-4">
+                        <FaFacebook className="text-3xl"></FaFacebook>
+                        <FaInstagram className="text-3xl"></FaInstagram>
+                        <FaLinkedin className="text-3xl"></FaLinkedin>
+                        <FaTwitter className="text-3xl"></FaTwitter>
+                        
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
