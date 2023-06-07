2ae18db2ac3fe5386f0efe9fe260e7eef659ec81
sayeed083 <sayeedislam407@gmailcom>
2023-06-07 17:59:42 +0600

    Added Design and a eye button for seeing password in the login page

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -1,29 +1,47 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import image from "../../../assets/authentication.png"
 
 
 const Login = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+    
     return (
         <div>
             <div className="hero min-h-screen bg-base-200" style={{ backgroundImage: `url("${image}")` }}>
                 <div className="hero-content flex-col lg:flex-row">
+
                     <div className="text-center lg:text-left">
                         <img className="w-[720px]" src="https://i.postimg.cc/KzSc7YSh/Authentication-2.png" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <h1 className="text-5xl font-bold mb-5 text-center">Login now!</h1>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                                <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <input type={showPassword ? "text" : "password"} {...register("password", { required: true })} name="password" placeholder="password" className="input input-bordered absolute mt-3" />
+                                <p className="relative translate-x-60 -translate-y-1" onClick={handleShowPassword}>
+                                    {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+                                </p>
 
                             </div>
 
--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,7 +1,17 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import image from "../../../assets/authentication.png"
 
 const Register = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        console.log("sas");
+    }
+
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200" style={{ backgroundImage: `url("${image}")` }}>
@@ -10,45 +20,45 @@ const Register = () => {
                         <img className="w-[720px]" src="https://i.postimg.cc/KzSc7YSh/Authentication-2.png" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <h1 className="text-3xl font-bold mb-5 text-center">Register now!</h1>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" name="name" placeholder="Name" className="input input-bordered" />
+                                <input type="text" {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                                <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <input type="password" {...register("password", { required: true })} name="password" placeholder="password" className="input input-bordered" />
 
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Confirm Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="Confirm password" className="input input-bordered" />
+                                <input type="password" {...register("c-password", { required: true })} name="c-password" placeholder="Confirm password" className="input input-bordered" />
 
                             </div>
-                            
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">PhotoURl</span>
                                 </label>
-                                <input type="text" name="photoURL" placeholder="photoURL" className="input input-bordered" />
+                                <input type="text" {...register("photoURL", { required: true })} name="photoURL" placeholder="photoURL" className="input input-bordered" />
                             </div>
 
 
                             <div className="form-control mt-6">
-                                <input className="btn btn-primary" type="submit" value="Login" />
+                                <input className="btn btn-primary" type="submit" value="Register" />
                             </div>
                             <p><small>Already Have an Account? Please <Link className="text-green-400" to="/login">Login!</Link></small></p>
 
