7f4722e7499b4530a828014a6555ee109e6a9abd
sayeed083 <sayeedislam407@gmailcom>
2023-06-09 23:30:39 +0600

    Showed MY Classes For Instrctor Panel

--- src/Pages/Dashboard/Instructor/MyClasses/MyClasses.jsx
+++ src/Pages/Dashboard/Instructor/MyClasses/MyClasses.jsx
@@ -1,9 +1,108 @@
+import useClass from "../../../../hooks/useClass";
 
 
 const MyClasses = () => {
+    const [classes] = useClass()
+
+    const pendingClasses = classes.filter(pendSingleCls => pendSingleCls.status === 'pending')
+
+
     return (
-        <div className="bg-sky-400">
-            <h2>This is My Class from Instructor</h2>
+        <div className="">
+            <h2 className="text-center text-5xl mb-10">My Classes</h2>
+            {/* ================ */}
+
+
+
+
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>SI No</th>
+                            <th>Class Name</th>
+                            <th>Instructor Name</th>
+                            <th>Email</th>
+                            <th>Available Seats</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                            <th>Enrolled Students</th>
+                            <th>Feedback</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            classes.map((sClass, index) => <tr key={sClass._id}>
+                                <th>{index + 1}</th>
+                                <td>{sClass.name}</td>
+                                <td>{sClass.instructor}</td>
+                                <td>{sClass.email}</td>
+                                <td>{sClass.availableSeats}</td>
+                                <td>{sClass.price}</td>
+                                <td>{sClass.status}</td>
+                                <td>{sClass.students}</td>
+                                <td>
+                                    {sClass.status === "denied" ? (
+                                        sClass.feedback
+                                    ) : (
+                                        <span>No feedback</span>
+                                    )}
+                                </td>
+
+
+
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* ================ */}
+
+
         </div>
     );
 };
